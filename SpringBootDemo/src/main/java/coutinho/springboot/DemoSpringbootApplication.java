989e2d684f5663af02210f7349fd87bb071f9704
William Coutinho <william.csouza@hotmail.com>
2023-09-18 22:27:51 -0300

    Projeto simples para aprendizado do beans,component e padrões prototype, singleton

---
+++ SpringBootDemo/src/main/java/coutinho/springboot/DemoSpringbootApplication.java
@@ -0,0 +1,23 @@
+package coutinho.springboot;
+
+import coutinho.springboot.app.SistemaMensagem;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemoSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringbootApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner run(SistemaMensagem sistemaMensagem) throws Exception {
+        return args -> {
+            sistemaMensagem.enviarMensagemBoasVindas();
+            sistemaMensagem.enviarConfirmacaoCadastro();
+        };
+    }
+}
