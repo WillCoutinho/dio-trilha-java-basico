42f5eb1e134e50945eaa3faffdc3c390670be64c
William Coutinho <william.csouza@hotmail.com>
2023-09-20 11:08:26 -0300

    Reorganizando pastas de estudos

--- SpringDataJPA/src/main/java/com/coutinho/springdatajpa/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.coutinho.springdatajpa.repository;
-
-import com.coutinho.springdatajpa.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Usuario, Integer> {
-}
